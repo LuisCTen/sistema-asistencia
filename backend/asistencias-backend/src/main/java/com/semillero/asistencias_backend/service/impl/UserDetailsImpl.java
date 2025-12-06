360602d9df5fe5042ae8cc45f8ac282c78b83bfa
Carbajal Tinecela <lecarbajal@indracompany.com>
2025-12-06 16:58:58 -0500

    Se agregó en services/impl la clase UserDetailsImpl para implementar la interfaz UserDetailsService

---
+++ backend/asistencias-backend/src/main/java/com/semillero/asistencias_backend/service/impl/UserDetailsImpl.java
@@ -0,0 +1,25 @@
+package com.semillero.asistencias_backend.service.impl;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsImpl implements UserDetails{
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getPassword() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getUsername() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
