c292c98f2a4b41317e5df40ab14f7e2a6a05639c
Carbajal Tinecela <lecarbajal@indracompany.com>
2025-12-06 14:24:09 -0500

    Se agregó los paquetes para la arquitectura y clase Rol en model

---
+++ backend/asistencias-backend/src/main/java/com/semillero/asistencias_backend/models/Role.java
@@ -0,0 +1,29 @@
+package com.semillero.asistencias_backend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "rol")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+
+}
