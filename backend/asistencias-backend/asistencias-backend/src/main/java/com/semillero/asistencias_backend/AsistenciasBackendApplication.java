813c020b6e302a8bd75f7eae6f5eed8a54604321
Carbajal Tinecela <lecarbajal@indracompany.com>
2025-12-05 23:49:29 -0500

    feat: estructura inicial del proyecto (BD + Backend Setup)

---
+++ backend/asistencias-backend/asistencias-backend/src/main/java/com/semillero/asistencias_backend/AsistenciasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.semillero.asistencias_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsistenciasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsistenciasBackendApplication.class, args);
+	}
+
+}
