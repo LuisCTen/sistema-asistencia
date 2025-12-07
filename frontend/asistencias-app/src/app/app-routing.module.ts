d48f0355d5af54c009db8fea71cfa9f5a11cc4e7
Carbajal Tinecela <lecarbajal@indracompany.com>
2025-12-07 11:54:32 -0500

    Se agrego el canActivate para que se protega la ruta dashboard

--- frontend/asistencias-app/src/app/app-routing.module.ts
+++ frontend/asistencias-app/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { sessionGuard } from './core/guards/session.guard';
 
 const routes: Routes = [
   {
@@ -11,6 +12,7 @@ const routes: Routes = [
     path: 'dashboard',
     loadChildren: () =>
       import('./modules/dashboard/dashboard.module').then((m) => m.DashboardModule),
+      canActivate:[sessionGuard]
   },
   {
     path: '',
