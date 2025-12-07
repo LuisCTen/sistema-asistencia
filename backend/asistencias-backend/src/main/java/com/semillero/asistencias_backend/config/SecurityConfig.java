ad412c7786728d33ee34fc5fbd2fdce838f72baf
Carbajal Tinecela <lecarbajal@indracompany.com>
2025-12-06 21:20:08 -0500

    Se agrego el securityConfig establecera reglas de autenticacion y autorizacion

---
+++ backend/asistencias-backend/src/main/java/com/semillero/asistencias_backend/config/SecurityConfig.java
@@ -0,0 +1,82 @@
+package com.semillero.asistencias_backend.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity // Habilita @PreAuthorize en los controladores
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            // Deshabilitar CSRF (No necesario para APIs REST Stateless, No dependen de cookies de sesión)
+            .csrf(AbstractHttpConfigurer::disable)
+            
+            // Configurar CORS (Vital para Angular)
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            
+            // Definir rutas públicas y privadas
+            //Define las reglas de autorización de URL.
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/**").permitAll() // Login público(acceso sin token)
+                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll() // Docs públicos(acceso sin token)
+                .anyRequest().authenticated() // El resto requiere Token(presentar un JWT válido) es decir autenticado.
+            )
+            
+            // 4. Gestión de sesión STATELESS (Política JWT estricta)--no se debe crear ni usar peticiones http
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            
+            // 5. Agregar nuestro filtro JWT antes del filtro estándar de usuario/pass
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    //se usa para cifrar las claves antes de que se almacenen en la base de datos (registro) y para compararlas durante el proceso de login.
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:4200")); // Origen Angular
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true);
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    
+}
