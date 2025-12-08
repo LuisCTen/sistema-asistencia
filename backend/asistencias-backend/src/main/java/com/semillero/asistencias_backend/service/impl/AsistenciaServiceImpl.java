771e2a2199aea274ce4d59e3f4c9faf8ebe727c8
Carbajal Tinecela <lecarbajal@indracompany.com>
2025-12-08 03:28:25 -0500

    Se implemento los metodos de mi contrato para llamar a los SP(storesprocedures) estos llaman asu vez al metodo ejecutarProcedimiento()-Pendiente:CRUD(JPA)

--- backend/asistencias-backend/src/main/java/com/semillero/asistencias_backend/service/impl/AsistenciaServiceImpl.java
+++ backend/asistencias-backend/src/main/java/com/semillero/asistencias_backend/service/impl/AsistenciaServiceImpl.java
@@ -3,9 +3,12 @@ package com.semillero.asistencias_backend.service.impl;
 import org.springframework.stereotype.Service;
 
 import com.semillero.asistencias_backend.dto.AsistenciaResponseDto;
+import com.semillero.asistencias_backend.mappers.AsistenciaMapper;
+import com.semillero.asistencias_backend.repository.IAsistenciaRepository;
 import com.semillero.asistencias_backend.service.IAsistenciaService;
 
 import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -13,14 +16,30 @@ import lombok.RequiredArgsConstructor;
 public class AsistenciaServiceImpl implements IAsistenciaService{
 
     private final EntityManager entityManager;
-
+    private final IAsistenciaRepository iAsistenciaRepository;
+    private final AsistenciaMapper asistenciaMapper;
+    
+    //--------------------------------------------------------------------------------------------
+    //Metodos que implementan la logica de mi interfaz
     @Override
+    @Transactional//Maneja la transaccion a la bd
     public AsistenciaResponseDto registrarEntrada(Long idUsuario) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        String mensajeOracle = ejecutarProcedimiento("PKG_ASISTENCIAS.SP_REGISTRAR_ENTRADA", idUsuario);
+        //Nos ayudamos del mapper para estructurar respuesta
+        return asistenciaMapper.toDtoFromSp(mensajeOracle, idUsuario) ;
     }
 
     @Override
     public AsistenciaResponseDto registrarSalida(Long idUsuario) {
+        String mensajeOracle =ejecutarProcedimiento("PKG_ASISTENCIAS.SP_REGISTRAR_SALIDA", idUsuario);
+        return asistenciaMapper.toDtoFromSp(mensajeOracle, idUsuario);
+    }
+    //--------------------------------------------------------------------------------------------
+    //Este metodo llama al Store Procedure
+
+    private String ejecutarProcedimiento(String nombreSp, Long idUsuario) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
 }
+
