d7fc75ae805f8f354ceb604b4fe19537371f4e53
Carbajal Tinecela <lecarbajal@indracompany.com>
2025-12-08 05:10:00 -0500

    Se intento acceder a marcar asistencia pero pide autenticacion

--- backend/asistencias-backend/src/main/java/com/semillero/asistencias_backend/controller/AsistenciaController.java
+++ backend/asistencias-backend/src/main/java/com/semillero/asistencias_backend/controller/AsistenciaController.java
@@ -1,8 +1,13 @@
 package com.semillero.asistencias_backend.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.semillero.asistencias_backend.dto.AsistenciaResponseDto;
 import com.semillero.asistencias_backend.service.IAsistenciaService;
 
 import lombok.RequiredArgsConstructor;
@@ -14,6 +19,12 @@ public class AsistenciaController {
 
     private final IAsistenciaService iAsistenciaService;
 
-    
+
+    @PostMapping("/marcar-asistencia/{username}")
+    public ResponseEntity<AsistenciaResponseDto> marcarAsistencia(@PathVariable String username){
+        
+        return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(iAsistenciaService.registrarEntrada(username));
+    }
 
 }
