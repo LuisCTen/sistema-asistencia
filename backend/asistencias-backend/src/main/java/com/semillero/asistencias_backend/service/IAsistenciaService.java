6c065d5792d8fdcbba37480021f54dcf858fd5a9
Carbajal Tinecela <lecarbajal@indracompany.com>
2025-12-08 04:27:08 -0500

    Se cambio el tipo de parametro antes(id)-->ahora(username)

--- backend/asistencias-backend/src/main/java/com/semillero/asistencias_backend/service/IAsistenciaService.java
+++ backend/asistencias-backend/src/main/java/com/semillero/asistencias_backend/service/IAsistenciaService.java
@@ -4,6 +4,7 @@ import com.semillero.asistencias_backend.dto.AsistenciaResponseDto;
 
 public interface IAsistenciaService {
     //Metodos para llamar a PLSQL
-    AsistenciaResponseDto registrarEntrada(Long idUsuario);
-    AsistenciaResponseDto registrarSalida (Long idUsuario);
+    //Hubo un cambio ahora recibe un username en vez del ID
+    AsistenciaResponseDto registrarEntrada(String username);
+    AsistenciaResponseDto registrarSalida (String username);
 }
