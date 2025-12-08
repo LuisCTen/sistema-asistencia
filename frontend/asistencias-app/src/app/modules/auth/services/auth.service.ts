3a82b6ccdc8520e3d420c2d16c24642ff42290a4
Carbajal Tinecela <lecarbajal@indracompany.com>
2025-12-08 17:15:25 -0500

    Se agrego comentarios y se verifico -todoOK

--- frontend/asistencias-app/src/app/modules/auth/services/auth.service.ts
+++ frontend/asistencias-app/src/app/modules/auth/services/auth.service.ts
@@ -9,13 +9,14 @@ import { environment } from 'src/environments/environment';
 })
 export class AuthService {
   private readonly URL=environment.api;
+  //Enviar peticion de login a la API -Almacenar token en navegador
   constructor(private http:HttpClient,private cookie:CookieService){}
 
   sendCredentials(username:string,password:string):Observable<any>{
     const body={username,password};
     return this.http.post(`${this.URL}/auth/login`,body)
     .pipe(
-      tap((response:any) =>{
+      tap((response:any) =>{//Interceptar flujo de Observable
         const {token} =response;
         //Guardamos token y le damos 4 días de validez.
         this.cookie.set('token',token,4,'/');
