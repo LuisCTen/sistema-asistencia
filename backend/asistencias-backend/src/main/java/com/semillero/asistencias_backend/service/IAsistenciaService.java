382e11a85e266f650ed721a290f1752b4d085dbb
Carbajal Tinecela <lecarbajal@indracompany.com>
2025-12-08 03:24:08 -0500

    Se creo el contrato que implementara el servicio, aqui estan los metodos para llamar al PlSQL

--- backend/asistencias-backend/src/main/java/com/semillero/asistencias_backend/service/IAsistenciaService.java
+++ backend/asistencias-backend/src/main/java/com/semillero/asistencias_backend/service/IAsistenciaService.java
@@ -3,6 +3,7 @@ package com.semillero.asistencias_backend.service;
 import com.semillero.asistencias_backend.dto.AsistenciaResponseDto;
 
 public interface IAsistenciaService {
+    //Metodos para llamar a PLSQL
     AsistenciaResponseDto registrarEntrada(Long idUsuario);
     AsistenciaResponseDto registrarSalida (Long idUsuario);
 }
