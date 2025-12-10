d4d032843912ef4b94b6183d1675ee007b85bfe0
Carbajal Tinecela <lecarbajal@indracompany.com>
2025-12-10 11:55:28 -0500

    Se creo el body del pkg_reportes

--- database/packages/pkg_reportes.sql
+++ database/packages/pkg_reportes.sql
@@ -18,3 +18,28 @@ CREATE OR REPLACE PACKAGE pkg_reportes AS
    --
 --
 END pkg_reportes;
+CREATE OR REPLACE PACKAGE BODY pkg_reportes AS
+   PROCEDURE sp_listar_historial(p_id_usuario IN usuario.id_usuario        %TYPE,
+                                 p_fec_inicio IN asistencia.fec_asistencia %TYPE DEFAULT NULL,
+                                 p_fec_fin    IN asistencia.fec_asistencia %TYPE DEFAULT NULL,
+                                 p_cursor    OUT SYS_REFCURSOR
+   )
+IS 
+   BEGIN
+      OPEN p_cursor FOR
+         SELECT 
+            a.id_asistencia,
+            a.fec_asistencia,
+            a.hora_entrada,
+            a.hora_salida,
+            a.estado_asistencia
+         FROM   asistencia a
+         WHERE  a.id_usuario = p_id_usuario
+            AND (p_fec_inicio IS NULL OR a.fec_asistencia>=p_fec_inicio)
+            AND (p_fec_fin    IS NULL OR a.fec_asistencia>=p_fec_fin   ) 
+         ORDER BY a.fec_asistencia DESC,a.hora_entrada DESC;
+    EXCEPTION
+       WHEN OTHERS THEN
+          OPEN p_cursor FOR SELECT NULL FROM dual WHERE 1=0;--sería mejor guardarlo en un tabla de log
+       END sp_listar_historial;
+END pkg_reportes;
