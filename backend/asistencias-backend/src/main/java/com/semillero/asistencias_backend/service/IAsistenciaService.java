fd02191c09b8d10e129e17091d54dcbc67d38566
Carbajal Tinecela <lecarbajal@indracompany.com>
2025-12-12 16:52:12 -0500

    Se proceió a agregar un metodo al contrato para que pueda ser implementado

--- backend/asistencias-backend/src/main/java/com/semillero/asistencias_backend/service/IAsistenciaService.java
+++ backend/asistencias-backend/src/main/java/com/semillero/asistencias_backend/service/IAsistenciaService.java
@@ -1,10 +1,16 @@
 package com.semillero.asistencias_backend.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.semillero.asistencias_backend.dto.AsistenciaResponseDto;
+import com.semillero.asistencias_backend.dto.HistorialRequestDto;
+import com.semillero.asistencias_backend.dto.HistorialResponseDto;
 
 public interface IAsistenciaService {
     //Metodos para llamar a PLSQL
     //Hubo un cambio ahora recibe un username en vez del ID
     AsistenciaResponseDto registrarEntrada(String username);
     AsistenciaResponseDto registrarSalida (String username);
+    Page<HistorialResponseDto> listarHistorial(String username,HistorialRequestDto filtros,Pageable pageable);
 }
