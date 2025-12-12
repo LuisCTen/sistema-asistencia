cec9c1903892c95d92b77d26d3ebd5bd98d323ee
Carbajal Tinecela <lecarbajal@indracompany.com>
2025-12-12 16:32:16 -0500

    Se agrego el sharedModule (imports)

--- frontend/asistencias-app/src/app/modules/dashboard/dashboard.module.ts
+++ frontend/asistencias-app/src/app/modules/dashboard/dashboard.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { HistoryPageComponent } from './pages/history-page/history-page.component';
 
 
 @NgModule({
   declarations: [
-    DashboardPageComponent
+    DashboardPageComponent,
+    HistoryPageComponent
   ],
   imports: [
     CommonModule,
