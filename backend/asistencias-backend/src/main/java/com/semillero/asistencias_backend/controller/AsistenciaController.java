0a5bb4e748d031b2fd8a7291d4fe3087a9e5f27d
Carbajal Tinecela <lecarbajal@indracompany.com>
2025-12-12 16:56:47 -0500

    Se agrego al controlador para realizar el metodo get y poder mostrar el historial -OK y en el http todo ok

--- backend/asistencias-backend/src/main/java/com/semillero/asistencias_backend/controller/AsistenciaController.java
+++ backend/asistencias-backend/src/main/java/com/semillero/asistencias_backend/controller/AsistenciaController.java
@@ -1,14 +1,20 @@
 package com.semillero.asistencias_backend.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.semillero.asistencias_backend.dto.AsistenciaResponseDto;
+import com.semillero.asistencias_backend.dto.HistorialRequestDto;
+import com.semillero.asistencias_backend.dto.HistorialResponseDto;
 import com.semillero.asistencias_backend.service.IAsistenciaService;
 
 import lombok.RequiredArgsConstructor;
@@ -35,5 +41,14 @@ public class AsistenciaController {
         return ResponseEntity.status(HttpStatus.OK)
                     .body(iAsistenciaService.registrarSalida(username));
     }
+    
+    @PreAuthorize("hasRole('EMPLEADO') or hasRole('ADMIN')")
+    @GetMapping("/historial")
+    public ResponseEntity<Page<HistorialResponseDto>> obtenerHistorial(@ModelAttribute HistorialRequestDto filtros,Pageable pageable){
+        String username =SecurityContextHolder.getContext().getAuthentication().getName();
+        return ResponseEntity.status(HttpStatus.OK)
+                    .body(iAsistenciaService.listarHistorial(username, filtros, pageable));
+    }
+
 
 }
