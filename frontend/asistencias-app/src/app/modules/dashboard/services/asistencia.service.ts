6b1894576c2dcfaf31dcd88f00c1eb793e68ee5d
Carbajal Tinecela <lecarbajal@indracompany.com>
2025-12-12 16:36:27 -0500

    Se agrego el metodo para poder obtener el historial de asistencias

--- frontend/asistencias-app/src/app/modules/dashboard/services/asistencia.service.ts
+++ frontend/asistencias-app/src/app/modules/dashboard/services/asistencia.service.ts
@@ -1,7 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AsistenciaResponse } from 'src/app/core/models/asistencia-response.model';
+import { HistorialResponse } from 'src/app/core/models/historial-response.model';
+import { Page } from 'src/app/core/models/page.model';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -24,5 +26,18 @@ export class AsistenciaService {
       `${this.URL}/asistencias/marcar-salida`,
       {}
     );
+  }
+  //--------------------------------------------------------------------------------------//
+  obtenerHistorial(page:number =0,size:number=5,fecInicio?:string,fecFin?:string):
+        Observable<Page<HistorialResponse>>{
+
+          let params = new HttpParams().set('page',page.toString())
+                                       .set('size',size.toString());
+
+        if(fecInicio) params = params.set('fechaInicio', fecInicio);
+        if(fecFin) params = params.set('fechaFin', fecFin);
+
+        return this.http.get<Page<HistorialResponse>>(`${this.URL}/asistencias/historial`,{params});
+
   }
 }
