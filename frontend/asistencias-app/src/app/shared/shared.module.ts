e7dd30b795cbab4173bece7c369b6b3a9c6075ec
Carbajal Tinecela <lecarbajal@indracompany.com>
2025-12-12 16:35:08 -0500

    Se actulizo importaciones y exportaciones para que otros modulos lo pueda usar

--- frontend/asistencias-app/src/app/shared/shared.module.ts
+++ frontend/asistencias-app/src/app/shared/shared.module.ts
@@ -3,13 +3,21 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HeaderUserComponent } from './components/header-user/header-user.component';
 import { RouterModule } from '@angular/router'; // routerLink
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [SidebarComponent, HeaderUserComponent],
   imports: [
     CommonModule,
     RouterModule, // No olvidar que es necesario para que funcione el menú
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  exports: [SidebarComponent, HeaderUserComponent],
+  exports: [
+    SidebarComponent,
+    HeaderUserComponent,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class SharedModule {}
