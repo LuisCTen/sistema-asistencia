b83325784042bab73786c4e66b57cd608e4d82f7
Carbajal Tinecela <lecarbajal@indracompany.com>
2025-12-13 10:50:52 -0500

    Se agrega el componente de justificaciones, disponible para ser usado dentro de los paths del DashboardRoutingModule y otros componentes de este modulo

--- frontend/asistencias-app/src/app/modules/dashboard/dashboard.module.ts
+++ frontend/asistencias-app/src/app/modules/dashboard/dashboard.module.ts
@@ -5,12 +5,14 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HistoryPageComponent } from './pages/history-page/history-page.component';
+import { JustificationPageComponent } from './pages/justification-page/justification-page.component';
 
 
 @NgModule({
   declarations: [
     DashboardPageComponent,
-    HistoryPageComponent
+    HistoryPageComponent,
+    JustificationPageComponent
   ],
   imports: [
     CommonModule,
