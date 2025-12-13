0a495560689d0433a2f14d699bc0e18e20fa8944
Carbajal Tinecela <lecarbajal@indracompany.com>
2025-12-13 10:25:27 -0500

    Se modifico el metodo para buscar id

--- backend/asistencias-backend/src/main/java/com/semillero/asistencias_backend/service/impl/AsistenciaServiceImpl.java
+++ backend/asistencias-backend/src/main/java/com/semillero/asistencias_backend/service/impl/AsistenciaServiceImpl.java
@@ -21,12 +21,10 @@ import com.semillero.asistencias_backend.dto.AsistenciaResponseDto;
 import com.semillero.asistencias_backend.dto.HistorialRequestDto;
 import com.semillero.asistencias_backend.dto.HistorialResponseDto;
 import com.semillero.asistencias_backend.exception.BadRequestException;
-import com.semillero.asistencias_backend.exception.ResourceNotFoundException;
 import com.semillero.asistencias_backend.mappers.AsistenciaMapper;
-import com.semillero.asistencias_backend.models.UserEntity;
 import com.semillero.asistencias_backend.repository.IAsistenciaRepository;
-import com.semillero.asistencias_backend.repository.IUserRepository;
 import com.semillero.asistencias_backend.service.IAsistenciaService;
+import com.semillero.asistencias_backend.service.IUsuarioService;
 
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
@@ -38,21 +36,12 @@ public class AsistenciaServiceImpl implements IAsistenciaService {
 
     private final EntityManager entityManager;
     private final IAsistenciaRepository iAsistenciaRepository;
-    private final IUserRepository iUserRepository;
+    private final IUsuarioService iUsuarioService;
     private final AsistenciaMapper asistenciaMapper;
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     //--------------------------------------------------------------------------------------------
-    //Metodo para buscarIdPorNombre
-    private Long buscarIdPorUsername(String username) {
-
-        UserEntity userEntity = iUserRepository.findByUsername(username)
-                .orElseThrow(() -> new ResourceNotFoundException("Usuario no encontrado"));
-
-        return userEntity.getIdUsuario();
-    }
-
     //--------------------------------------------------------------------------------------------
     //Metodos que implementan la logica de mi interfaz
     @Override
@@ -60,15 +49,15 @@ public class AsistenciaServiceImpl implements IAsistenciaService {
     public AsistenciaResponseDto registrarEntrada(String username) {
 
         //Buscar el id del usuario
-        Long idUsuario = buscarIdPorUsername(username);
+        Long idUsuario = iUsuarioService.buscarIdPorUsername(username);
 
         //Llamar al SP
         String mensajeOracle = ejecutarProcedimiento("SP_REGISTRAR_ENTRADA", idUsuario);
 
-        	if (mensajeOracle.startsWith("ERROR")) {
+        if (mensajeOracle.startsWith("ERROR")) {
 
-                throw new BadRequestException(mensajeOracle); 
-	        }
+            throw new BadRequestException(mensajeOracle);
+        }
         //Nos ayudamos del mapper para estructurar respuesta
         return asistenciaMapper.toDtoFromSp(mensajeOracle, idUsuario);
     }
@@ -76,14 +65,14 @@ public class AsistenciaServiceImpl implements IAsistenciaService {
     @Override
     public AsistenciaResponseDto registrarSalida(String username) {
         //LLama al metodo y busca id por usuario
-        Long idUsuario = buscarIdPorUsername(username);
+        Long idUsuario = iUsuarioService.buscarIdPorUsername(username);
 
         //Lama al SP
         String mensajeOracle = ejecutarProcedimiento("SP_REGISTRAR_SALIDA", idUsuario);
-                	if (mensajeOracle.startsWith("ERROR")) {
+        if (mensajeOracle.startsWith("ERROR")) {
 
-                throw new BadRequestException(mensajeOracle); 
-	        }
+            throw new BadRequestException(mensajeOracle);
+        }
         return asistenciaMapper.toDtoFromSp(mensajeOracle, idUsuario);
     }
     //--------------------------------------------------------------------------------------------
@@ -93,7 +82,7 @@ public class AsistenciaServiceImpl implements IAsistenciaService {
 
         try {
             SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate)
-                    .withSchemaName("ASISTENCIA_SCHEMA") 
+                    .withSchemaName("ASISTENCIA_SCHEMA")
                     .withCatalogName("PKG_ASISTENCIAS") // nombre del paquete
                     .withProcedureName(nombreSp) // nombre del SP sin paquete
                     .declareParameters(
@@ -112,70 +101,66 @@ public class AsistenciaServiceImpl implements IAsistenciaService {
 
     @Override
     public Page<HistorialResponseDto> listarHistorial(String username, HistorialRequestDto filtros, Pageable pageable) {
-        Long idUsuario =buscarIdPorUsername(username);
+        Long idUsuario = iUsuarioService.buscarIdPorUsername(username);
 
-        
         //Convertimos el LocalDate  sql.data y manejando nulos.
-        Date sqlInicio = (filtros.getFechaInicio()!=null) ? Date.valueOf(filtros.getFechaInicio()) :null;
-        Date sqlFin = (filtros.getFechaFin()!=null) ? Date.valueOf(filtros.getFechaFin()) :null;
-        
-        System.out.println("🔎 DEBUG: Buscando historial para Usuario ID: " + idUsuario);
-    System.out.println("🔎 DEBUG: Filtro Inicio: " + sqlInicio);
-    System.out.println("🔎 DEBUG: Filtro Fin: " + sqlFin);
+        Date sqlInicio = (filtros.getFechaInicio() != null) ? Date.valueOf(filtros.getFechaInicio()) : null;
+        Date sqlFin = (filtros.getFechaFin() != null) ? Date.valueOf(filtros.getFechaFin()) : null;
 
-        try{
+        System.out.println(" DEBUG: Buscando historial para Usuario ID: " + idUsuario);
+        System.out.println(" DEBUG: Filtro Inicio: " + sqlInicio);
+        System.out.println("DEBUG: Filtro Fin: " + sqlFin);
+
+        try {
             //Llamar al SP del package
-            SimpleJdbcCall jdbcCall= new SimpleJdbcCall(jdbcTemplate)
-                                .withCatalogName("PKG_REPORTES")
-                                .withProcedureName("SP_LISTAR_HISTORIAL")
-                                .declareParameters(
-                                    new SqlParameter("p_id_usuario",Types.NUMERIC),
-                                    new SqlParameter("p_fec_inicio",Types.DATE),
-                                    new SqlParameter("p_fec_fin",Types.DATE),
-                                    new SqlParameter("p_page",Types.NUMERIC),
-                                    new SqlParameter("p_size",Types.NUMERIC),
+            SimpleJdbcCall jdbcCall = new SimpleJdbcCall(jdbcTemplate)
+                    .withCatalogName("PKG_REPORTES")
+                    .withProcedureName("SP_LISTAR_HISTORIAL")
+                    .declareParameters(
+                            new SqlParameter("p_id_usuario", Types.NUMERIC),
+                            new SqlParameter("p_fec_inicio", Types.DATE),
+                            new SqlParameter("p_fec_fin", Types.DATE),
+                            new SqlParameter("p_page", Types.NUMERIC),
+                            new SqlParameter("p_size", Types.NUMERIC),
+                            //Tenemos que tener mapeado al cursor
+                            new SqlOutParameter("p_cursor", Types.REF_CURSOR, new RowMapper<HistorialResponseDto>() {
+                                @Override
+                                public HistorialResponseDto mapRow(java.sql.ResultSet rs, int rowNum) throws java.sql.SQLException {
+                                    return HistorialResponseDto.builder()
+                                            .idAsistencia(rs.getLong("ID_ASISTENCIA"))
+                                            .fecAsistencia(rs.getDate("FEC_ASISTENCIA"))
+                                            .horaEntrada(rs.getTimestamp("HORA_ENTRADA"))
+                                            .horaSalida(rs.getTimestamp("HORA_SALIDA"))
+                                            .estadoAsistencia(rs.getString("ESTADO_ASISTENCIA"))
+                                            .totalRegistros(rs.getLong("TOTAL_REGISTROS"))
+                                            .build();
+                                }
+                            })
+                    );
 
-                                    //Tenemos que tener mapeado al cursor
-                                    new SqlOutParameter("p_cursor", Types.REF_CURSOR,new RowMapper<HistorialResponseDto>() {
-                                        @Override
-                                        public HistorialResponseDto mapRow (java.sql.ResultSet rs, int rowNum) throws  java.sql.SQLException{
-                                        return HistorialResponseDto.builder()
-                                                .idAsistencia(rs.getLong("ID_ASISTENCIA"))
-                                                .fecAsistencia(rs.getDate("FEC_ASISTENCIA"))
-                                                .horaEntrada(rs.getTimestamp("HORA_ENTRADA"))
-                                                .horaSalida(rs.getTimestamp("HORA_SALIDA"))
-                                                .estadoAsistencia(rs.getString("ESTADO_ASISTENCIA"))
-                                                .totalRegistros(rs.getLong("TOTAL_REGISTROS"))
-                                                .build();
-                                        } 
-                                    })
-                                );
+            Map<String, Object> params = new HashMap<>();
+            params.put("p_id_usuario", idUsuario);
+            params.put("p_fec_inicio", sqlInicio);
+            params.put("p_fec_fin", sqlFin);
+            params.put("p_page", pageable.getPageNumber());
+            params.put("p_size", pageable.getPageSize());
 
-            Map<String,Object> params =new HashMap<>();
-                params.put("p_id_usuario", idUsuario);
-                params.put("p_fec_inicio",sqlInicio);
-                params.put("p_fec_fin",sqlFin);
-                params.put("p_page",pageable.getPageNumber());
-                params.put("p_size",pageable.getPageSize());
-                
-            Map<String,Object> result =jdbcCall.execute(params);
+            Map<String, Object> result = jdbcCall.execute(params);
 
             //Obtener la lista
-            List<HistorialResponseDto> lista =(List<HistorialResponseDto>) result.get("p_cursor");
+            List<HistorialResponseDto> lista = (List<HistorialResponseDto>) result.get("p_cursor");
 
-            long total=0;
-            if(!lista.isEmpty()){
+            long total = 0;
+            if (!lista.isEmpty()) {
                 total = lista.get(0).getTotalRegistros();
             }
 
-            return new PageImpl<>(lista,pageable,total);
-        }catch(Exception e){
-                System.err.println("Error en el historial(paginacion): "+e.getMessage());
-                return Page.empty(pageable);
+            return new PageImpl<>(lista, pageable, total);
+        } catch (Exception e) {
+            System.err.println("Error en el historial(paginacion): " + e.getMessage());
+            return Page.empty(pageable);
         }
     }
-
-    
 
     /*       try{
             StoredProcedureQuery query =entityManager.createStoredProcedureQuery(nombreSp);
