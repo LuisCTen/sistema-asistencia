ddd21c75c009c23fb798e6ed45c3f79a4e5a6567
Carbajal Tinecela <lecarbajal@indracompany.com>
2025-12-13 10:21:24 -0500

    Se agrego al pkg 2 procedures uno para listar justificaciones y otro para aprobar o desaprobar

--- database/packages/pkg_justificacion.sql
+++ database/packages/pkg_justificacion.sql
@@ -3,6 +3,13 @@ CREATE OR REPLACE PACKAGE pkg_justificaciones AS
                                         p_fec_incidencia   IN  justificacion.fec_incidencia %TYPE ,
                                         p_motivo           IN  justificacion.motivo           %TYPE ,
                                         p_resultado       OUT  VARCHAR2                             );
+   --Lista todas las justificaciones pendientes (ADMIN)
+   PROCEDURE sp_listar_pendientes(p_cursor OUT SYS_REFCURSOR);
+   
+   --Aprobar o rechazar justificacion pendiente
+   PROCEDURE sp_atender_solicitud(p_id_justificacion  IN justificacion.id_justificacion %TYPE   ,
+                                   p_estado_nuevo     IN justificacion. estado_solicitud %TYPE  ,
+                                   p_resultado       OUT VARCHAR2                               );
 END  pkg_justificaciones;                               
 /
 CREATE OR REPLACE PACKAGE BODY pkg_justificaciones AS
@@ -40,5 +47,61 @@ CREATE OR REPLACE PACKAGE BODY pkg_justificaciones AS
       ROLLBACK;
       p_resultado :='Error'||SQLERRM;
    END sp_registrar_justificacion;
+   --
+   --
+   PROCEDURE sp_listar_pendientes(p_cursor OUT SYS_REFCURSOR)
+   IS
+   BEGIN 
+      --
+      OPEN p_cursor FOR 
+         --
+         SELECT 
+            a.id_justificacion,
+            b.username as nombre_usuario,
+            a.fec_incidencia,
+            a.motivo,
+            a.fec_solicitud
+         FROM justificacion a
+         INNER JOIN usuario b
+         ON    a.id_usuario  = b.id_usuario
+         WHERE a.estado_solicitud = 'PENDIENTE'
+         ORDER BY a.fec_solicitud ASC; --Para poner adelante los mas antiguos
+         --
+      --
+   END sp_listar_pendientes;
+   --Aprobar o rechazar solicitud
+   PROCEDURE sp_atender_solicitud(
+    p_id_justificacion IN justificacion.id_justificacion %TYPE   ,
+    p_estado_nuevo     IN justificacion. estado_solicitud %TYPE  ,
+    p_resultado       OUT VARCHAR2                               )
+   IS 
+   BEGIN 
+      --
+      IF p_estado_nuevo NOT IN ('APROBADO','RECHAZADO') THEN
+         --
+         p_resultado := 'ERROR: Estado Invalido.Use aprobado o rechazado';
+         RETURN;
+         --
+      END IF;
+      --
+      UPDATE justificacion
+      SET estado_solicitud =p_estado_nuevo
+      WHERE id_justificacion = p_id_justificacion;
+      --
+      IF SQL%ROWCOUNT = 0 THEN
+         --
+         p_resultado :='ERROR: No se encontró la justificación';
+         --
+      ELSE
+         --
+         COMMIT;
+         p_resultado := 'OK. Solicitud actualizada a '||p_estado_nuevo;
+         --
+      END IF;
+   --
+   EXCEPTION WHEN OTHERS THEN
+      ROLLBACK;
+      p_resultado :='Error'||SQLERRM;
+   END sp_atender_solicitud;
    --
 END pkg_justificaciones;
