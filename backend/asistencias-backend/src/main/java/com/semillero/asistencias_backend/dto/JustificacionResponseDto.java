decee63d03b1cb456e181fa2407b2b556e48082e
Carbajal Tinecela <lecarbajal@indracompany.com>
2025-12-12 21:24:23 -0500

    Cambio en el bear y agregue comentario en el response

--- backend/asistencias-backend/src/main/java/com/semillero/asistencias_backend/dto/JustificacionResponseDto.java
+++ backend/asistencias-backend/src/main/java/com/semillero/asistencias_backend/dto/JustificacionResponseDto.java
@@ -1,5 +1,11 @@
 package com.semillero.asistencias_backend.dto;
 
-public class JustificacionResponseDto {
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@Builder
+public class JustificacionResponseDto {
+    private String mensaje;
+    //Aqui se tmabien puede retornar el id de la solicitud(justificacion)
 }
