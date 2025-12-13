4a37fe4da641aedd9b657719165d2baaf803bf16
Carbajal Tinecela <lecarbajal@indracompany.com>
2025-12-13 10:53:17 -0500

    Se agrego la ruta para permitir el acceso al componente de justificaciones

--- frontend/asistencias-app/src/app/modules/dashboard/dashboard-routing.module.ts
+++ frontend/asistencias-app/src/app/modules/dashboard/dashboard-routing.module.ts
@@ -2,23 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 import { HistoryPageComponent } from './pages/history-page/history-page.component';
+import { JustificationPageComponent } from './pages/justification-page/justification-page.component';
 
 const routes: Routes = [
   {
-    path:'',
-    component:DashboardPageComponent,
-    children:[
+    path: '',
+    component: DashboardPageComponent,
+    children: [
       //Aqui irá history, reports (como hijos)
-    ]
+    ],
   },
-    {
-    path:'history',
-    component:HistoryPageComponent,
-  }
+  {
+    path: 'history',
+    component: HistoryPageComponent,
+  },
+  {
+    path: 'justification',
+    component: JustificationPageComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {}
